dd17a2ad777c9539c4c7124aaab3d1dd86dc8727
Artem <severin.tema@gmail.com>
2023-10-15 12:35:22 +0300

    fix js

--- js/index.js
+++ js/index.js
@@ -13,17 +13,35 @@ const images = [
   "10.jpg",
 ];
 
-const container = document.querySelector(".container");
-const card = document.createElement("div");
+function rendomImages() {
+  const randomImg = Math.floor(Math.random() * images.length);
 
-images.forEach(() => {
-  const randomImg = images[Math.floor(Math.random() * images.length)];
+  return images[randomImg];
+}
+
+function createImgElem(src) {
   const image = document.createElement("img");
-  image.src = `images/${randomImg}`;
+
+  image.src = src;
   image.setAttribute("width", "400");
   image.setAttribute("height", "300");
   image.classList.add("img");
-  card.append(image);
-});
 
-container.append(card);
+  return image;
+}
+
+function renderImg(images) {
+  const container = document.querySelector(".container");
+  const card = document.createElement("div");
+
+  images.forEach(() => {
+    const randomImg = rendomImages();
+    const imageElement = createImgElem(`images/${randomImg}`);
+
+    card.append(imageElement);
+  });
+
+  container.append(card);
+}
+
+renderImg(images);
